2ce6b1e68ab92f8b8d48a85861a81916b75c1560
Musa Shaba <musamohshaba@gmail.com>
2023-08-01 17:54:56 +0100

    The site works, left for media queries

---
+++ assets/scripts/main.js
@@ -0,0 +1,32 @@
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+const success = document.getElementById('success');
+const mainPage = document.getElementById('main-container');
+const mailLink = document.getElementById('place-mail');
+const invalid = document.getElementById('invalid');
+
+let regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+)[.]([a-z]{2,4})$/;
+
+function renderSucces() {
+  mailLink.innerHTML = email.value;
+  mainPage.classList.add('active');
+  success.classList.add('active');
+}
+function renderError() {
+  email.classList.add('active');
+  invalid.style.display = 'unset';
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (regx.test(email.value)) {
+    renderSucces();
+  } else {
+    renderError();
+  }
+});
+
+function dismiss() {
+  mainPage.classList.remove('active');
+  success.classList.remove('active');
+}
